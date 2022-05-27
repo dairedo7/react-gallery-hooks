8d62abbc743afe75b2e613bee51898493a5c1392
Vladyslav Svitana <vladik719@gmail.com>
2022-05-27 10:24:25 +0200

    Update API.js

--- src/components/Services/API.js
+++ src/components/Services/API.js
@@ -4,7 +4,6 @@ axios.defaults.baseURL = 'https://pixabay.com/api';
 const API_KEY = '24546244-2fe39103bf24816f7c49d8399';
 
 export const fetchPicturesWithQuery = async (searchQuery, page) => {
-  page += 1;
   const response = await axios.get(
     `?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
